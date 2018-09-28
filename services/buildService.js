c683c086b01039eef15d2831a6439c10b6472947
Nathan Smith <nathansmith@Nathans-MacBook-Pro.local>
2018-09-28 18:40:34 -0400

    Improved log, made exec's happen in blocking order

--- services/buildService.js
+++ services/buildService.js
@@ -4,41 +4,94 @@ const path = require('path');
 const logger = require('../logger');
 
 module.exports = async (repo, repoPath) => {
-    logger.info(`Building ${repo} in ${repoPath}...`);
+    logger.info(`[START]=== Building ${repo} in ${repoPath}... ===[START]`);
 
-    // reset any changes that have been made locally
-    exec(`git -C ${repoPath} reset --hard`, execCallback);
-
-    // and ditch any files that have been added locally too
-    exec(`git -C ${repoPath} clean -df`, execCallback);
-
-    // now pull down the latest
-    exec(`git -C ${repoPath} pull -f`, execCallback);
-
-    // and npm install with --production
-    exec(`npm -C ${repoPath} install --production`, execCallback);
+    await executeGitCommands(repoPath);
+    await executeInstallProductionModules(repoPath);
+    await executePostBuildScripts(repoPath);
 
     // restart server
     exec(`pm2 restart ${repo}`);
 
-    logger.info(`Build successful for ${repo} in ${repoPath}`);
-
-    await executePostBuildScripts(repoPath);
+    logger.info(`[END]=== Build successful for ${repo} in ${repoPath} ===[END]`);
 };
 
+
+/**
+ * Logs err, stdout, or stderr to logfile
+ * @param {*} err
+ * @param {*} stdout
+ * @param {*} stderr
+ */
 function execCallback(err, stdout, stderr) {
     if (err) logger.error(err);
     if (stdout) logger.info(stdout);
     if (stderr) logger.error(stderr);
 }
 
+/**
+ * Executes all post build scripts in package.json with the fieldname 'ns-postbuild'
+ * @param {String} repoPath path of the project on the server
+ */
 async function executePostBuildScripts(repoPath) {
     const file = await fs.readFile(path.resolve(repoPath, 'package.json'));
     const json = JSON.parse(file);
 
     const postbuildScript = json.scripts['ns-postbuild'];
-    if (postbuildScript) {
-        logger.info(`Executing post build script for: ${postbuildScript}`);
-        exec(postbuildScript, { cwd: repoPath });
-    }
+    return new Promise((resolve, reject) => {
+        if (postbuildScript) {
+            logger.info(`--- Executing post build script for: ${postbuildScript} ---`);
+            exec(postbuildScript, { cwd: repoPath }, (err, stdout, stderr) => {
+                execCallback(err, stdout, stderr);
+                if (err || stderr) reject(err || stderr);
+                resolve();
+            });
+        }
+    });
+}
+
+/**
+ * Resets local changes, clears local changes, pulls latest files from github
+ * @param {String} repoPath path of the project on the server
+ */
+async function executeGitCommands(repoPath) {
+    logger.info('--- Executing Git Commands ---');
+
+    return new Promise((resolve, reject) => {
+        logger.info('Resetting any changes that have been made locally');
+        exec(`git -C ${repoPath} reset --hard`, (err, stdout, stderr) => {
+            execCallback(err, stdout, stderr);
+            if (err || stderr) reject(err || stderr);
+
+            logger.info('Clearing locally made changes.');
+            exec(`git -C ${repoPath} clean -df`, (err, stdout, stderr) => {
+                execCallback(err, stdout, stderr);
+                if (err || stderr) reject(err || stderr);
+
+                logger.info('Pulling latest code from repository');
+                exec(`git -C ${repoPath} pull -f`, (err, stdout, stderr) => {
+                    execCallback(err, stdout, stderr);
+                    if (err || stderr) reject(err || stderr);
+
+                    resolve();
+                });
+            });
+        });
+    });
+}
+
+/**
+ * Installs production npm modules
+ * @param {String} repoPath path of the project on the server
+ */
+async function executeInstallProductionModules(repoPath) {
+    logger.info('--- Installing Production Modules ---');
+    return new Promise((resolve, reject) => {
+        exec(`npm -C ${repoPath} install --production`, (err, stdout, stderr) => {
+            execCallback(err, stdout, stderr);
+            if (err || stderr) reject(err || stderr);
+
+            resolve();
+        });
+    });
 }
